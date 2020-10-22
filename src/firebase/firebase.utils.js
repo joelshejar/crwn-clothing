55a57fdc2ed885c584cb379534ce0bb60db84125
abc <joel.rajesh13@gmail.com>
2020-10-22 22:56:57 +0530

    brought shop data back from firestore to app

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -53,6 +53,23 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
   return await batch.commit();
 };
 
+export const convertCollectionsSnapshotToMap =(collections) => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routename: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    }
+  });
+
+  console.log(transformedCollection);
+
+}
+
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
